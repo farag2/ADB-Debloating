d5ce074b2c4e1a9743f41589354277ee4863a27d
Dmitry Nefedov <nefedovds@ya.ru>
2022-07-07 12:11:08 +0300

    Update OneUI.cmd

--- OneUI.cmd
+++ OneUI.cmd
@@ -257,8 +257,8 @@ echo Яндекс
 %~dp0adb kill-server
 echo.
 
-taskkill /F /IM adb.exe
+taskkill /F /IM adb.exe /T 2>NUL
 
-RMDIR /S /Q %USERPROFILE%\.android
-RMDIR /S /Q %USERPROFILE%\.dbus-keyrings
+RMDIR /S /Q %USERPROFILE%\.android 2>NUL
+RMDIR /S /Q %USERPROFILE%\.dbus-keyrings 2>NUL
 pause
