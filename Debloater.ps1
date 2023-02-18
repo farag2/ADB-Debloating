a3ea5f19bc2c9c2086d6c034fe6556bbc36c4865
Sunset <grigory.orlov.set@gmail.com>
2023-02-18 09:41:54 +0500

    unify quoting style

--- Debloater.ps1
+++ Debloater.ps1
@@ -2,8 +2,8 @@
 #Requires -Version 5.1
 
 #region Preparations
-$ErrorActionPreference = 'Stop'
-[Console]::Title = 'ADB Debloater'
+$ErrorActionPreference = "Stop"
+[Console]::Title = "ADB Debloater"
 Remove-Module -Name Functions -ErrorAction SilentlyContinue
 Import-Module -Name $PSScriptRoot\Functions.ps1
 #endregion Preparations
