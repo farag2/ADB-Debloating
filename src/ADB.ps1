fab103a4bb7f7771dd8c06cd40bc14ef67a5f310
Dmitry Nefedov <nefedovds@ya.ru>
2023-10-14 15:21:45 +0300

    Delete src/ADB.ps1

--- src/ADB.ps1
+++
@@ -1,4 +0,0 @@
-# ADB
-New-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\usbflags\18D1D00D0100 -Name SkipContainerIdQuery -PropertyType Binary -Value ([byte[]](01, 00, 00, 00)) -Force
-New-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\usbflags\18D1D00D0100 -Name SkipBOSDescriptorQuery -PropertyType Binary -Value ([byte[]](01, 00, 00, 00)) -Force
-New-ItemProperty -Path HKLM:\SYSTEM\CurrentControlSet\Control\usbflags\18D1D00D0100 -Name osvc -PropertyType Binary -Value ([byte[]](00, 00)) -Force
